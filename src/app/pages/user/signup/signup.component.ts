9889f92eb764141b4825155b4f9f4f5cc2a1f99a
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-08-14 18:30:54 +0700

    update 8 files

--- src/app/pages/user/signup/signup.component.ts
+++ src/app/pages/user/signup/signup.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from '@env/environment';
 
 @Component({
-	selector: 'pg-user-login',
-	templateUrl: './login.component.html',
-	styleUrl: './login.component.sass',
+	selector: 'pg-signup',
+	templateUrl: './signup.component.html',
+	styleUrl: './signup.component.sass',
 })
-export class LoginComponent implements OnInit {
+export class SignupComponent implements OnInit {
 	form!: FormGroup;
 	loading = false;
 	submitted = false;
@@ -22,8 +22,10 @@ export class LoginComponent implements OnInit {
 
 	ngOnInit() {
 		this.form = this.formBuilder.group({
+			firstName: ['', Validators.required],
+			lastName: ['', Validators.required],
 			email: ['', Validators.required],
-			password: ['', Validators.required],
+			password: ['', [Validators.required, Validators.minLength(6)]],
 		});
 	}
 
@@ -33,24 +35,26 @@ export class LoginComponent implements OnInit {
 
 	onSubmit() {
 		this.submitted = true;
+
 		if (this.form.invalid) return;
 
 		this.loading = true;
 		this.httpSvc
 			.post(
-				environment.backendUrl('/auth/login'),
+				environment.backendUrl('/auth/signup'),
 				{
 					email: this.f['email'].value,
 					password: this.f['password'].value,
+					firstName: this.f['firstName'].value,
+					lastName: this.f['lastName'].value,
 				},
 				{ withCredentials: true }
 			)
 			.subscribe({
-				next: (req: any) => {
-					if (req.success) this.router.navigateByUrl('/user');
+				next: (value: any) => {
+					if (value.success) this.router.navigateByUrl('/user');
 				},
 				error: (err) => {
-					console.error(err.error.message);
 					this.loading = false;
 				},
 			});
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,14 +1,23 @@
-import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
+// Components
 import { CoreComponent } from './core.component';
-import { AppComponent } from '../app/app.component';
-import { SidenavComponent } from '../app/components/sidenav/sidenav.component';
-import { MapComponent } from '../app/components/map/map.component';
+import { AppComponent } from '@app/app.component';
+import { SidenavComponent } from '@cp/sidenav/sidenav.component';
+import { MapComponent } from '@cp/map/map.component';
+import { InfoComponent } from '@pg/user/info/info.component';
+import { FameComponent } from '@pg/fame/fame.component';
+import { LuckComponent } from '@pg/luck/luck.component';
+import { UserComponent } from '@pg/user/user.component';
+import { FortuneComponent } from '@pg/fortune/fortune.component';
+import { LoginComponent } from '@pg/user/login/login.component';
+import { SignupComponent } from '@pg/user/signup/signup.component';
 // Angular's modules
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 // materials' module
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -16,27 +25,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FameComponent } from '../app/pages/fame/fame.component';
-import { LuckComponent } from '../app/pages/luck/luck.component';
-import { UserComponent } from '../app/pages/user/user.component';
-import { FortuneComponent } from '../app/pages/fortune/fortune.component';
-import { LoginComponent } from '../app/pages/user/login/login.component';
-import { CommonModule } from '@angular/common';
-import { InfoComponent } from 'src/app/pages/user/info/info.component';
 
 @NgModule({
 	declarations: [
 		// Components
 		CoreComponent,
 		AppComponent,
+		// Untils
 		SidenavComponent,
 		MapComponent,
+		// @pg
 		FameComponent,
 		LuckComponent,
-		UserComponent,
 		FortuneComponent,
 		FameComponent,
+		// User
+		UserComponent,
 		LoginComponent,
+		SignupComponent,
 		InfoComponent,
 	],
 	imports: [
