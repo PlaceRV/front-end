685595f8364a0bda1efb3a24b17b587b01824ce8
takahashinguyen <vietuk.nguyen.git@icloud.com>
2024-09-15 03:42:04 +0000

    delete 1 file and update 1 file

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component({ selector: 'cp-loading', templateUrl: './loading.component.html' })
+@Component({
+	selector: 'cp-loading',
+	template: `
+		<div class="fr h-full w-full items-center justify-center">
+			<span class="ds-loading ds-loading-spinner w-1/12 text-primary"></span>
+		</div>
+	`,
+})
 export class LoadingComponent {}
